48136137dfa9fae64aa0a44532763328302fc685
Lou Pendley <17365150+loupendley@users.noreply.github.com>
2018-12-26 10:57:05 -0800

    Initial checkin to Github.

--- LearningPython/LearningAllAboutObjects.py
+++ LearningPython/LearningAllAboutObjects.py
@@ -1,24 +1,55 @@
 #   This is from the book Python For Evrybody, by Charles R. Severance,
 #   Page 178
 
+from party import PartyAnimal
 
-class PartyAnimal:
-    x = 0
+#     x = 0
+#
+#     def __init__(self):
+#         print("I am constructed.")
+#
+#     def party(self):
+#         self.x = self.x + 1
+#         print("So far", self.x)
+#
+#     def __del__(self):
+#         print('I am destructed', self.x)
 
 
-    def party(self):
-        self.x = self.x + 1
-        print("So far", self.x)
-
-
-an = PartyAnimal()
-print("the Type of an is {}".format(type(an)))
-print("the Dir of an is {}".format(dir(an)))
-print("the type  of an.x is {}".format(type(an.x)))
-print("the Type of type(an.party) is {}".format(type(an.party)))
+# an = PartyAnimal()
+# an.party()
+# an.party()
+# an = 42
+# print('an contains', an)
+# print("the Type of an is {}".format(type(an)))
+# print("the Dir of an is {}".format(dir(an)))
+# print("the type  of an.x is {}".format(type(an.x)))
+# print("the Type of type(an.party) is {}".format(type(an.party)))
 # an.party()
 # an.party()
 # an.party()
 # PartyAnimal.party(an)
+
+# class PartyAnimal:
+#     x = 0
+#     name = ''
+#
+#     def __init__(self, nam):
+#         self.name = nam
+#         print(self.name, 'constructed')
+#
+#     def party(self):
+#         self.x = self.x + 1
+#         print(self.name, 'attended party number', self.x)
+
+
+
+s = PartyAnimal('Sylvia')
+j = PartyAnimal('Jim')
+
+s.party()
+j.party()
+s.party()
+
 
 
