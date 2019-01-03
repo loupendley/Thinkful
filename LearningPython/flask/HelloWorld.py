c9265cae88f7ddf2e7abfc3d7e2c51b2395cd01b
loupendley <loupendley@users.noreply.github.com>
2019-01-02 23:49:15 -0800

    Changed flask ip address?

--- LearningPython/flask/HelloWorld.py
+++ LearningPython/flask/HelloWorld.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")  # take note of this decorator syntax, it's a common pattern
 def hello():
-    return "Wello Harold!"
+    return "Wello Harold!, and while we are at it, let's just talk about all of the fun things that we did in 2018.  \"" \
+           "Well we did go to the beach, and have a lot of fun, that is for sure."
 
 
 if __name__ == "__main__":
